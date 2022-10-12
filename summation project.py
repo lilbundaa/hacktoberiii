089fdb103271be50ef0f7c392d300deb6dbc4cbf
Muhammad Aisy Afrah <110968070+AisyAfr@users.noreply.github.com>
2022-10-12 10:16:24 +0700

    Create summation project.py

---
+++ summation project.py
@@ -0,0 +1,5 @@
+#summation
+number1 = input('first number:')
+number2 = input('second number:')
+sum = int(number1) + int(number2)
+print ('Hasil penjumlahan {0} dan {1} adalah {2}'.format(number1,number2,sum))
